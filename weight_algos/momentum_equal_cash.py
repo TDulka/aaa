22a91d6401ee86eb94208cbb0e1735a4d6a0d3f2
Tomas <Personal@Tomass-MacBook-Pro.local>
2021-01-07 15:45:31 +0100

    refactor: simplify, even more pythonish

--- weight_algos/momentum_equal_cash.py
+++ weight_algos/momentum_equal_cash.py
@@ -5,16 +5,9 @@ def compute_weights_momentum_equal_cash(n_month_returns, last_month_daily_return
     stocks = [{ 'ticker': ticker, 'n_month_return': n_month_returns[ticker] } for ticker in n_month_returns]
     stocks.sort(key = lambda x: x['n_month_return'], reverse=True)
     
-    best_half = stocks[0:top50percent]   
+    selected_stocks = [x['ticker'] for x in stocks[0:top50percent] if x['n_month_return'] > 1]
 
-    weights = { ticker: 0 for ticker in n_month_returns }
-    weights['CASH'] = 0 
+    weights = dict([(ticker, weight) if ticker in selected_stocks else (ticker, 0) for ticker in n_month_returns])
+    weights['CASH'] = (top50percent - len(selected_stocks)) * weight
     
-    for entry in best_half:
-        ticker = entry['ticker']
-        if n_month_returns[ticker] > 1:
-            weights[ticker] = weight
-        else:
-            weights['CASH'] += weight
-
     return weights
