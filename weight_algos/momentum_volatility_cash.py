d4960637c53f275f1d9ac31adf860afd77f1a4b5
Tomas <Personal@Tomass-MacBook-Pro.local>
2021-01-07 15:44:58 +0100

    fix: correctly allocate the cash proportion in volatility weighting

--- weight_algos/momentum_volatility_cash.py
+++ weight_algos/momentum_volatility_cash.py
@@ -3,25 +3,20 @@ def compute_weights_momentum_volatility_cash(n_month_returns, last_month_daily_r
     
     stocks = [{ 'ticker': ticker, 'n_month_return': n_month_returns[ticker] } for ticker in n_month_returns]
     stocks.sort(key = lambda x: x['n_month_return'], reverse=True)
-    
-    best_half = stocks[0:top50percent]   
+    selected_stocks = [x['ticker'] for x in stocks[0:top50percent] if x['n_month_return'] > 1]
 
-    weights = { ticker: 0 for ticker in n_month_returns }
-    weights['CASH'] = 0 
-    
     volatilities = {}
     for ticker in last_month_daily_returns:
         samplemean = sum(last_month_daily_returns[ticker]) / 20
         samplevariance = sum((x - samplemean) ** 2 for x in last_month_daily_returns[ticker]) / 20
         volatilities[ticker] = samplevariance ** 0.5 
 
-    total_volatility_of_best = sum([ volatilities[stock['ticker']] for stock in best_half ])
+    total_volatility_of_best = sum([ volatilities[ticker] for ticker in selected_stocks ])
 
-    for entry in best_half:
-        ticker = entry['ticker']
-        if n_month_returns[ticker] > 1:
-            weights[ticker] = volatilities[ticker] / total_volatility_of_best
-
-    weights['CASH'] = 1 - sum([weights[ticker] for ticker in weights])
+    weights = { ticker: 0 for ticker in n_month_returns }
+    weights['CASH'] = (top50percent - len(selected_stocks)) * (1 / top50percent)
+    
+    for ticker in selected_stocks:
+        weights[ticker] = (1 - weights['CASH']) * volatilities[ticker] / total_volatility_of_best
 
     return weights
